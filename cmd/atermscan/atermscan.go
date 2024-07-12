d7c10061d6e465d4bae2595839e401b6184109a2
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-07-13 00:52:29 +0900

    refactoring scanner

--- cmd/atermscan/atermscan.go
+++ cmd/atermscan/atermscan.go
@@ -10,36 +10,56 @@ import (
 	"github.com/koron-go/atermsearch"
 )
 
-func scan(ctx context.Context, deviceTimeout time.Duration) (<-chan *atermsearch.Device, error) {
+type Scanner struct {
+	verbose       bool
+	deviceTimeout time.Duration
+}
+
+func (s Scanner) Scan(ctx context.Context) (<-chan *atermsearch.Device, error) {
 	ch := make(chan *atermsearch.Device)
-	go func() {
-		var wg sync.WaitGroup
-		for i := 0; i < 256; i++ {
-			wg.Add(1)
-			go func(addr string) {
-				defer wg.Done()
-				ctx2, cancel := context.WithTimeout(ctx, deviceTimeout)
-				defer cancel()
-				dev, err := atermsearch.Search(ctx2, addr)
-				if err != nil {
-					//log.Printf("[INFO] %s: %s", addr, err)
-					return
-				}
-				ch <- dev
-			}(fmt.Sprintf("192.168.1.%d", i))
-		}
-		wg.Wait()
-		close(ch)
-	}()
+	go s.scanAsync(ctx, ch)
 	return ch, nil
 }
 
+func (s Scanner) scanAsync(ctx context.Context, ch chan<- *atermsearch.Device) {
+	var wg sync.WaitGroup
+	for i := 0; i < 256; i++ {
+		wg.Add(1)
+		addr := fmt.Sprintf("192.168.1.%d", i)
+		go func() {
+			s.search(ctx, ch, addr)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	close(ch)
+}
+
+func (s Scanner) search(ctx0 context.Context, ch chan<- *atermsearch.Device, addr string) {
+	ctx, cancel := context.WithTimeout(ctx0, s.deviceTimeout)
+	defer cancel()
+	dev, err := atermsearch.Search(ctx, addr)
+	if err != nil {
+		if s.verbose {
+			log.Printf("[INFO] %s: %s", addr, err)
+		}
+		return
+	}
+	ch <- dev
+}
+
 func main() {
-	ctx := context.Background()
-	devices, err := scan(ctx, 5*time.Second)
+	scanner := Scanner{
+		verbose:       false,
+		deviceTimeout: 5 * time.Second,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	devices, err := scanner.Scan(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	const format = "%-15s\t%-15s\t%-24s\n"
 	fmt.Printf(format, "Address", "Product Name", "Mode")
 	for dev := range devices {
